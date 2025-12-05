188ee9ee10f3cf61daf2f1c290f5764a43f99cd2
Fangqun-Gao <139853348+gfjgfjgfj@users.noreply.github.com>
2025-12-06 07:20:50 +0800

    Delete rag directory

--- rag/constance.py
+++
@@ -1,9 +0,0 @@
-from config import gemini_key
-from config import pinecone_key
-from rag.vector.PineconeStore import PineconeStore
-from rag.vector.library_vector import LibraryVectorUtil
-from rag.kg.builder_library_kg import LibraryKGBuilder
-
-libraryKg = LibraryKGBuilder()
-libraryVector = LibraryVectorUtil()
-
